2363cf17c830a94ba329f153e0d81af2d37e07f6
mateusmaciel340 <mateusmaciel340@gmail.com>
2023-05-22 23:32:00 -0300

    feat: introdução ao javascript

---
+++ 01-meuPrimeiroPrograma.js
@@ -0,0 +1,25 @@
+console.log('-----------------------------------');
+
+const nome = 'Mateus';
+const sobrenome = 'Maciel';
+const idade = 22;
+
+console.log('Olá, ' + nome + '.');
+console.log('Sobrenome: ' + sobrenome + '.');
+console.log('Você tem ' + idade + ' anos.');
+
+console.log('-----------------------------------');
+
+const primeiroValor = 3;
+const segundoValor = 5;
+console.log('O valor da constante primeiro valor: ' + primeiroValor);
+
+const somandoMaisDois = primeiroValor + segundoValor;
+console.log(primeiroValor + ' + ' + 2 + ' = ' + somandoMaisDois);
+
+console.log('-----------------------------------');
+
+console.log(primeiroValor + ' + ' + segundoValor + ' = ' + (primeiroValor + segundoValor));
+console.log(primeiroValor + ' - ' + segundoValor + ' = ' + (primeiroValor - segundoValor));
+console.log(primeiroValor + ' * ' + segundoValor + ' = ' + (primeiroValor * segundoValor));
+console.log(primeiroValor + ' / ' + segundoValor + ' = ' + (primeiroValor / segundoValor));
---
+++ 02-trabalhandoComVariaveis.js
@@ -0,0 +1,10 @@
+console.log('-----------------------------------');
+
+const nome = 'Mateus';
+const sobrenome = 'Maciel';
+const idade = 22;
+
+console.log('Nome: ' + nome + ' ' + sobrenome + ' - Idade: ' + idade);
+
+console.log('-----------------------------------');
+
